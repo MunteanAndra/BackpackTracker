79772133c1a74a355776f33747efed0f9813745c
andramuntean <munteanandraoana@yahoo.com>
2023-05-21 23:15:39 +0300

    updated location fetching from firebase

--- src/UI/Components/LocationHandler.js
+++ src/UI/Components/LocationHandler.js
@@ -3,41 +3,61 @@ import {useCallback, useEffect, useState} from "react";
 import {GOOGLE_API_KEY} from "../../mapsAPI";
 import {collection, getDocs} from "firebase/firestore";
 import {db} from "../../firebase";
-
-/*import Geocoder from "react-geocode";
-
-Geocoder.setApiKey(GOOGLE_API_KEY);
-Geocoder.setRegion("ro");
-Geocoder.setLocationType("ROOFTOP");
-
-Geocoder.fromLatLng("48.8583701", "2.2922926").then(
-    (response) => {
-        const address = response.results[0].formatted_address;
-        console.log(address);
-    },
-    (error) => {
-        console.error(error);
-    }
-);*/
+import {onValue, ref} from "firebase/database";
 
 const containerStyle = {
     width: '100%',
     height: '100%'
 };
 
-const gpsLocation = {
-    lat: 45.74754,
-    lng: 21.226026
-};
-
-export const LocationHandler = () => {
+export const LocationHandler = ({func}) => {
 
     const [coordinates, setCoordinates] = useState([]);
     const [map, setMap] = useState(null);
+    const [lat, setLat] = useState(0);
+    const [lng, setLng] = useState(0);
+    const [address, setAddress] = useState('');
+
+    const gpsLocation = {
+        lat: lat,
+        lng: lng,
+    };
 
     useEffect(() => {
         fetchLocation();
+        onValue(ref(db, '/lat'), (snapshot) => {
+            setLat(0);
+            const data = snapshot.val();
+            if (data !== null) {
+                setLat(data);
+            }
+        });
+        onValue(ref(db, '/long'), (snapshot) => {
+            setLng(0);
+            const data = snapshot.val();
+            if (data !== null) {
+                setLng(data);
+            }
+        });
+        convertToAddress().then(r => {func(address);});
+        console.log(address);
     }, []);
+
+    const convertToAddress = async () => {
+        try {
+            const response = await fetch(
+                `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=AIzaSyCWj9OaziQiK004WYa73Paqr7bvbqhgLNM`
+            );
+            const data = await response.json();
+            if (data.status === 'OK') {
+                setAddress(data.results[0].formatted_address);
+            } else {
+                setAddress('Unable to retrieve address');
+            }
+        } catch (error) {
+            setAddress('Error occurred while fetching address');
+        }
+    };
 
     const fetchLocation = async () => {
         await getDocs(collection(db, "coordinates"))
--- src/UI/Pages/ShowLocation.js
+++ src/UI/Pages/ShowLocation.js
@@ -2,11 +2,20 @@ import {Box, Grid} from "@mui/material";
 import pinIcon from "../../images/pinIcon.png";
 import {BlackButton} from "../Components/CustomButtons/BlackButton";
 import {LocationHandler} from "../Components/LocationHandler";
+import {useState} from "react";
 
 export const ShowLocation = () => {
+
+    const [address, setAddress] = useState('');
+
+    const getAddress = (data) => {
+        console.log('Data received from child:', data);
+        setAddress(data);
+    };
+
     return (
         <Grid container>
-            <Grid item xs={4} style={{ display: 'flex', flexDirection: 'column'}}>
+            <Grid item xs={4} style={{display: 'flex', flexDirection: 'column'}}>
                 <Grid item
                       xs={12}
                       style={{
@@ -37,12 +46,9 @@ export const ShowLocation = () => {
                           flexDirection: 'column',
                           alignItems: 'center',
                           justifyContent: 'center',
-                          paddingTop: '3rem',
                       }}
                 >
-                    <BlackButton>
-                        See Alert
-                    </BlackButton>
+                    {address}
                 </Grid>
                 <Grid item
                       xs={12}
@@ -51,11 +57,12 @@ export const ShowLocation = () => {
                           flexDirection: 'column',
                           alignItems: 'center',
                           justifyContent: 'center',
+                          paddingTop: '3rem',
                       }}
                 >
-                    <div>
-                        Location Message
-                    </div>
+                    <BlackButton>
+                        See Alert
+                    </BlackButton>
                 </Grid>
             </Grid>
             <Grid item
@@ -70,7 +77,7 @@ export const ShowLocation = () => {
                       height: '30rem',
                   }}
             >
-                <LocationHandler />
+                <LocationHandler func={getAddress}/>
             </Grid>
         </Grid>
     );
