eb26562782855b5687a9fec12e9133529a8efdf8
andramuntean <munteanandraoana@yahoo.com>
2023-05-24 09:46:56 +0300

    added images for adjusting straps

--- src/UI/Pages/Position.js
+++ src/UI/Pages/Position.js
@@ -3,6 +3,8 @@ import {useEffect, useState} from "react";
 import {onValue, ref} from "firebase/database";
 import {db} from "../../firebase";
 import strapsBackpack from "../../images/backpack_with_straps.png";
+import leftStrap from "../../images/leftStrap.png";
+import rightStrap from "../../images/rightStrap.png";
 
 export const Position = () => {
 
@@ -11,11 +13,15 @@ export const Position = () => {
     const [sensor3, setSensor3] = useState('');
     const [sensor4, setSensor4] = useState('');
 
+    let decideStrap;
     let leftStrapPressure = 0;
     let rightStrapPressure = 0;
+    let frontPressure = 0;
+    let backPressure = 0;
 
     let message = '';
     let secondMessage = '';
+    let thirdMessage = '';
 
     useEffect(() => {
         onValue(ref(db, '/pressureSensor1'), (snapshot) => {
@@ -50,6 +56,8 @@ export const Position = () => {
 
     leftStrapPressure = sensor1 + sensor2;
     rightStrapPressure = sensor3 + sensor4;
+    frontPressure = sensor1 + sensor3;
+    backPressure = sensor2 + sensor4;
 
     if (leftStrapPressure !== rightStrapPressure) {
         message = 'You are not properly wearing your backpack. Its position affects your back and in time it might cause you back pain. Readjust its straps and re-check the result!';
@@ -61,28 +69,47 @@ export const Position = () => {
         secondMessage = '';
     } else if ( leftStrapPressure < rightStrapPressure) {
         secondMessage = 'You might consider readjusting your right strap.';
+        decideStrap = 'true';
     } else {
         secondMessage = 'You might consider readjusting your left strap.';
+        decideStrap = 'false';
+    }
+
+    if (frontPressure === backPressure) {
+        thirdMessage = '';
+    } else if ( frontPressure < backPressure) {
+        thirdMessage = 'And you might also readjust your straps to the front because there is too much pressure on your back.';
+    } else {
+        thirdMessage = 'And you might also readjust your straps to the back because there is too much pressure on the front.';
     }
 
     return (
         <Grid container>
-            <Grid item xs={6}>
+            <Grid item xs={12} md={5}>
                 <img src={strapsBackpack} alt={"backpack"}/>
             </Grid>
-            <Grid item xs={6}
+            <Grid item xs={12} md={7}
                   style={{
                       padding: '0rem 3rem',
                       fontSize: '1.5rem',
                       fontWeight: '500',
                       display: 'flex',
                       alignItems: 'center',
+                      flexDirection: 'column',
                   }}
             >
-                {message}
-                <br></br>
-                <br></br>
-                {secondMessage}
+                <Grid item xs={12}>
+                    {message}
+                    <br></br>
+                    <br></br>
+                    {secondMessage}
+                    <br></br>
+                    <br></br>
+                    {thirdMessage}
+                </Grid>
+                <Grid item xs={12}>
+                    {decideStrap ? <img src={rightStrap} alt="rightStrap" /> : <img src={leftStrap} alt="leftStrap" />}
+                </Grid>
             </Grid>
             <Grid item
                   xs={12}
